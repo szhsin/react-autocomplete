f0251736181fdd1b063c8637ec67ddd37ca4b6e4
Zheng Song <41896553+szhsin@users.noreply.github.com>
2023-07-03 18:47:45 +1000

    Setup build pipeline

---
+++ babel.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  targets: 'defaults',
+  assumptions: {
+    constantReexports: true,
+    ignoreFunctionLength: true,
+    ignoreToPrimitiveHint: true,
+    iterableIsArray: true,
+    noDocumentAll: true,
+    noIncompleteNsImportDetection: true,
+    noNewArrows: true,
+    objectRestNoSymbols: true,
+    pureGetters: true,
+    setComputedProperties: true,
+    setSpreadProperties: true,
+    skipForOfIteratorClosing: true
+  },
+  shouldPrintComment: (val) => /[@#]__PURE__/.test(val),
+  plugins: ['pure-annotations'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        bugfixes: true,
+        include: [
+          '@babel/plugin-proposal-optional-chaining',
+          '@babel/plugin-proposal-nullish-coalescing-operator',
+          '@babel/plugin-proposal-optional-catch-binding'
+        ],
+        exclude: ['@babel/plugin-transform-typeof-symbol']
+      }
+    ],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    ['@babel/preset-typescript', { isTSX: true, allExtensions: true }]
+  ]
+};
