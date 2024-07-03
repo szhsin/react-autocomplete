c952724247c61c55cffbfc8e14ba09a55339a885
Zheng Song <szhsin9@gmail.com>
2024-07-04 00:43:50 +1000

    Clear input on ESC (#39)

--- dist/esm/features/atom/autocompleteLite.js
+++ dist/esm/features/atom/autocompleteLite.js
@@ -59,9 +59,9 @@ const autocompleteLite = ({
       onClick: () => {
         stopCapture();
         setOpen(true);
-        onChange('');
         setTmpValue();
         setFocusItem();
+        onChange('');
         if (deselectOnClear) onSelectChange();
       }
     }),
@@ -93,7 +93,7 @@ const autocompleteLite = ({
         }
       },
       onBlur: () => {
-        if (inCapture() || !open) return;
+        if (inCapture()) return;
         if (selectOnBlur && focusItem) {
           selectItemOrAction(focusItem, true);
         }
@@ -117,7 +117,12 @@ const autocompleteLite = ({
             }
             break;
           case 'Escape':
-            if (open) resetState(true);
+            if (open) {
+              resetState(true);
+            } else {
+              onChange('');
+              if (deselectOnClear) onSelectChange();
+            }
             break;
         }
       },
