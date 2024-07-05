62dd9577cf2457798be37d785ecc344d0c48c557
Zheng Song <szhsin9@gmail.com>
2024-07-05 22:29:39 +1000

    0.9.2

--- types/features/atom/inline.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Feature, GetPropsFunctions, FeatureProps } from '../../common';
-type AutoInlineFeature<T> = Feature<T, Pick<GetPropsFunctions<T>, 'getInputProps'>>;
-declare const autoInline: <T>({ getFocusItem }: Pick<FeatureProps<T>, "getFocusItem">) => AutoInlineFeature<T>;
-export { type AutoInlineFeature, autoInline };
