4407d4e6ad6137b5ad9abf93369f49cce759186b
Zheng Song <szhsin9@gmail.com>
2024-11-01 00:42:30 +1100

    Add mode selection in the base example

--- website/src/components/autocomplete/index.tsx
+++ website/src/components/autocomplete/index.tsx
@@ -2,17 +2,22 @@ import { useState, useRef, useEffect } from 'react';
 import { clsx } from 'clsx';
 import { useCombobox, autocompleteLite } from '@szhsin/react-autocomplete';
 import ClearIcon from '@site/static/img/x.svg';
+import { RadioButton } from '../Radio';
 import styles from './styles.module.css';
 
 const FRUITS = ['Apple', 'Banana', 'Blueberry', 'Cherry', 'Grape', 'Pineapple', 'Strawberry'];
 
+type Mode = 'select' | 'free';
+
 const Autocomplete = () => {
   const listRef = useRef<HTMLUListElement>(null);
+  const [mode, setMode] = useState<Mode>('select');
   const [value, setValue] = useState<string>();
   const [selected, setSelected] = useState<string>();
   const items = value
     ? FRUITS.filter((item) => item.toLowerCase().includes(value.toLowerCase()))
     : FRUITS;
+  const isSelectMode = mode === 'select';
 
   const {
     getInputProps,
@@ -21,14 +26,17 @@ const Autocomplete = () => {
     getItemProps,
     open,
     focusIndex,
-    isInputEmpty
+    isInputEmpty,
+    inputRef
   } = useCombobox({
     items,
     value,
     onChange: setValue,
-    selected,
-    onSelectChange: setSelected,
-    feature: autocompleteLite({ select: true })
+    feature: autocompleteLite({ select: isSelectMode }),
+    ...(isSelectMode && {
+      selected,
+      onSelectChange: setSelected
+    })
   });
 
   useEffect(() => {
@@ -39,10 +47,37 @@ const Autocomplete = () => {
     }
   }, [open, items.length]);
 
+  const handleModeChange = (mode: Mode) => {
+    setMode(mode);
+    setValue(undefined);
+    setSelected(undefined);
+    inputRef.current.focus();
+  };
+
   return (
     <div>
+      <div className={styles.modes}>
+        <RadioButton
+          name="mode"
+          value="select"
+          label="Select mode"
+          groupValue={mode}
+          onChange={handleModeChange}
+        />
+        <RadioButton
+          name="mode"
+          value="free"
+          label="Free mode"
+          groupValue={mode}
+          onChange={handleModeChange}
+        />
+      </div>
       <div className={styles.inputWrap}>
-        <input className={styles.input} placeholder="Pick a fruit..." {...getInputProps()} />
+        <input
+          className={styles.input}
+          placeholder={isSelectMode ? 'Pick a fruit...' : 'Type to search...'}
+          {...getInputProps()}
+        />
         {!isInputEmpty && (
           <button className={styles.clear} {...getClearProps()}>
             <ClearIcon />
