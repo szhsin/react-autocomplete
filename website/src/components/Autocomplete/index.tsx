da36d88cfdc83b9d0ae55fd5ab2e3e49376b98ce
Zheng Song <szhsin9@gmail.com>
2024-11-06 22:58:16 +1100

    autocomplete feature docs

--- website/src/components/Autocomplete/index.tsx
+++ website/src/components/Autocomplete/index.tsx
@@ -1,28 +1,30 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 import { clsx } from 'clsx';
-import { useCombobox, autocompleteLite } from '@szhsin/react-autocomplete';
+import { useCombobox, autocomplete } from '@szhsin/react-autocomplete';
 import ClearIcon from '@site/static/img/x.svg';
-import { RadioButton } from '../Radio';
+import ChevronDown from '@site/static/img/chevron-down.svg';
+import ChevronUp from '@site/static/img/chevron-up.svg';
+import STATES from '@site/src/data/states';
 import styles from '@site/src/css/styles.module.css';
 import customStyles from './styles.module.css';
-
-const FRUITS = ['Apple', 'Banana', 'Blueberry', 'Cherry', 'Grape', 'Pineapple', 'Strawberry'];
+import { RadioButton } from '../Radio';
 
 type Mode = 'select' | 'free';
 
-const Intro = () => {
-  const listRef = useRef<HTMLUListElement>(null);
+const Autocomplete = () => {
   const [mode, setMode] = useState<Mode>('select');
   const [value, setValue] = useState<string>();
   const [selected, setSelected] = useState<string>();
   const items = value
-    ? FRUITS.filter((item) => item.toLowerCase().includes(value.toLowerCase()))
-    : FRUITS;
+    ? STATES.filter((item) => item.toLowerCase().startsWith(value.toLowerCase()))
+    : STATES;
   const isSelectMode = mode === 'select';
 
   const {
+    getLabelProps,
     getInputProps,
     getClearProps,
+    getToggleProps,
     getListProps,
     getItemProps,
     open,
@@ -33,20 +35,12 @@ const Intro = () => {
     items,
     value,
     onChange: setValue,
-    feature: autocompleteLite({ select: isSelectMode }),
+    feature: autocomplete({ select: isSelectMode }),
     ...(isSelectMode && {
       selected,
       onSelectChange: setSelected
     })
   });
-
-  useEffect(() => {
-    if (open) {
-      if (listRef.current.getBoundingClientRect().bottom > window.innerHeight) {
-        listRef.current.scrollIntoView({ block: 'end', behavior: 'smooth' });
-      }
-    }
-  }, [open, items.length]);
 
   const handleModeChange = (mode: Mode) => {
     setMode(mode);
@@ -56,15 +50,8 @@ const Intro = () => {
   };
 
   return (
-    <div>
+    <div className={customStyles.wrap}>
       <div className={customStyles.modes}>
-        <RadioButton
-          name="mode"
-          value="select"
-          label="Select mode"
-          groupValue={mode}
-          onChange={handleModeChange}
-        />
         <RadioButton
           name="mode"
           value="free"
@@ -72,27 +59,43 @@ const Intro = () => {
           groupValue={mode}
           onChange={handleModeChange}
         />
-      </div>
-      <div className={customStyles.inputWrap}>
-        <input
-          className={styles.input}
-          placeholder={isSelectMode ? 'Pick a fruit...' : 'Type to search...'}
-          {...getInputProps()}
+        <div className={customStyles.desc}>
+          <i>(the default setting)</i> The text entered in the input field is not restricted to
+          the items in the dropdown list.
+        </div>
+        <RadioButton
+          name="mode"
+          value="select"
+          label="Select mode"
+          groupValue={mode}
+          onChange={handleModeChange}
         />
+        <div className={customStyles.desc}>
+          The allowed text in the input field is restricted to the items in the dropdown list.
+        </div>
+      </div>
+      <label {...getLabelProps()}>State</label>
+      <div className={styles.inputWrap}>
+        <input className={styles.input} placeholder="Select or type..." {...getInputProps()} />
         {!isInputEmpty && (
-          <button className={clsx(styles.iconBtn, customStyles.clear)} {...getClearProps()}>
+          <button className={styles.clear} {...getClearProps()}>
             <ClearIcon />
           </button>
         )}
+        <button className={styles.toggle} {...getToggleProps()}>
+          {open ? <ChevronUp /> : <ChevronDown />}
+        </button>
       </div>
 
       <ul
-        ref={listRef}
         className={styles.list}
         {...getListProps()}
         style={{
           display: open ? 'block' : 'none',
-          position: 'absolute'
+          position: 'absolute',
+          overflow: 'auto',
+          maxHeight: 300,
+          zIndex: 1
         }}
       >
         {items.length ? (
@@ -117,4 +120,4 @@ const Intro = () => {
   );
 };
 
-export { Intro };
+export { Autocomplete };
