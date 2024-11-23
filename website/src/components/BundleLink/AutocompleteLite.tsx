f3f092af354982ddb381f9d8b6ef093b1aa53a41
Zheng Song <szhsin9@gmail.com>
2024-11-23 15:33:41 +1100

    Fix the bundlejs.com link

--- website/src/components/BundleLink/AutocompleteLite.tsx
+++ website/src/components/BundleLink/AutocompleteLite.tsx
@@ -2,7 +2,7 @@ const Link = () => (
   <a
     target="_blank"
     rel="noopener noreferrer"
-    href="https://bundlejs.com/?q=%40szhsin%2Freact-autocomplete&bundle&treeshake=%5B%7B+useCombobox%2CautocompleteLite+%7D%5D&config=%7B%22esbuild%22%3A%7B%22external%22%3A%5B%22react%22%5D%7D%7D"
+    href="https://bundlejs.com/?q=%40szhsin%2Freact-autocomplete&treeshake=%5B%7B+useCombobox%2CautocompleteLite+%7D%5D&config=%7B%22esbuild%22%3A%7B%22external%22%3A%5B%22react%22%5D%7D%7D"
   >
     1.4 kB
   </a>
