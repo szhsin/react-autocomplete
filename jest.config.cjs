58f437ebc6b9078bc3de228fdfad96fd77d5e7e2
Zheng Song <szhsin9@gmail.com>
2025-07-06 14:52:05 +1000

    chore(deps): update all dependencies to latest versions (#87)

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,7 @@
 const config = {
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.[jt]s?(x)'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom', 'jest-dom-extended/jest'],
   clearMocks: true,
   collectCoverage: true
 };
