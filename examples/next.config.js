ffbe5d352e19d46b64414b8d6401f4ee3703ccd5
Zheng Song <szhsin9@gmail.com>
2025-10-26 20:14:02 +1100

    chore(examples): enable Turbopack in Next.js config

--- examples/next.config.js
+++ examples/next.config.js
@@ -1,6 +1,14 @@
+// @ts-check
+
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  turbopack: {
+    root: path.join(__dirname, '..')
+  }
 };
 
 module.exports = nextConfig;
