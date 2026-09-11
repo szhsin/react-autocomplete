c0515d76fdb3cce0d1e9270f72567d3c0a9eeb35
szhsin <41896553+szhsin@users.noreply.github.com>
2026-09-11 19:01:09 +0000

    chore(deps): update all dependencies

--- dist/esm/utils/mergeGroupedItems.mjs
+++
@@ -1,7 +0,0 @@
-//#region src/utils/mergeGroupedItems.ts
-const isArray = Array.isArray;
-const mergeGroupedItems = ({ groups, getItemsInGroup }) => {
-	return (isArray(groups) ? groups : Object.values(groups)).reduce((accu, group) => accu.concat(isArray(group) ? group : getItemsInGroup ? getItemsInGroup(group) : []), []);
-};
-//#endregion
-export { mergeGroupedItems };
